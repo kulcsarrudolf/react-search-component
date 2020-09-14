1539c47901efeef3eef7438700a68579e8093673
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2020-09-14 11:49:29 +0300

    React Testing Library Set-Up

---
+++ src/components/search/__test__/SearchByCarId.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import SearchCarById from "./../SearchCarById";
+
+it("test", () => {
+  render(<SearchCarById />);
+});
